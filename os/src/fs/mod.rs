d32d15dd38360a4d6529810e4e1cf677177bd251
wyfcyx <wyfcyx@gmail.com>
2019-12-10 07:42:46 +0800

    chk-ch9-part1

---
+++ os/src/fs/mod.rs
@@ -0,0 +1,50 @@
+mod device;
+
+use lazy_static::*;
+use rcore_fs::vfs::*;
+use rcore_fs_sfs::SimpleFileSystem;
+use alloc::{ sync::Arc, vec::Vec };
+
+lazy_static! {
+    pub static ref ROOT_INODE: Arc<dyn INode> = {
+        let device = {
+            extern "C" {
+                fn _user_img_start();
+                fn _user_img_end();
+            };
+            let start = _user_img_start as usize;
+            let end = _user_img_end as usize;
+            Arc::new(unsafe { device::MemBuf::new(start, end) })        
+        };
+        let sfs = SimpleFileSystem::open(device).expect("failed to open SFS");
+        sfs.root_inode()
+        
+    };
+}
+
+pub trait INodeExt {
+    fn read_as_vec(&self) -> Result<Vec<u8>>;
+}
+
+impl INodeExt for dyn INode {
+    fn read_as_vec(&self) -> Result<Vec<u8>> {
+        let size = self.metadata()?.size;
+        let mut buf = Vec::with_capacity(size);
+        unsafe {
+            buf.set_len(size);
+        }
+        self.read_at(0, buf.as_mut_slice())?;
+        Ok(buf)
+    }
+}
+
+pub fn init() {
+    println!("available programs in rust/ are:");
+    let mut id = 0;
+    let mut rust_dir = ROOT_INODE.lookup("rust").unwrap();
+    while let Ok(name) = rust_dir.get_entry(id) {
+        id += 1;
+        println!("  {}", name);
+    }
+    println!("++++ setup fs!        ++++")
+}
