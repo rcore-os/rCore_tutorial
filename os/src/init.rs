0c11e42a2174f0cc76a275bfe8815b7aa47d8bec
wyfcyx <wyfcyx@gmail.com>
2019-12-09 14:12:48 +0800

    chk-ch4-part1

--- os/src/init.rs
+++ os/src/init.rs
@@ -1,13 +1,44 @@
 global_asm!(include_str!("boot/entry64.asm"));
 
+use crate::consts::*;
+use crate::memory::{
+    alloc_frame,
+    dealloc_frame
+};
+
 #[no_mangle]
 pub extern "C" fn rust_main() -> ! {
+    extern "C" {
+        fn end();
+    }
+    println!(
+        "free physical memory paddr = [0x{:x}, 0x{:x})",
+        end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR,
+        PHYSICAL_MEMORY_END
+    );
+	println!(
+        "free physical memory ppn = [0x{:x}, 0x{:x})",
+        ((end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR) >> 12) + 1,
+        PHYSICAL_MEMORY_END >> 12
+	);
     crate::interrupt::init();
     crate::timer::init();
 
-    unsafe {
-        asm!("ebreak"::::"volatile");
-    }
-    panic!("end of rust_main");
+	crate::memory::init(
+        ((end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR) >> 12) + 1,
+        PHYSICAL_MEMORY_END >> 12
+    );
+    frame_allocating_test();
     loop {}
+}
+
+fn frame_allocating_test() {
+    println!("alloc {:#x?}", alloc_frame());
+    let f = alloc_frame();
+    println!("alloc {:#x?}", f);
+    println!("alloc {:#x?}", alloc_frame());
+    println!("dealloc {:#x?}", f);
+    dealloc_frame(f.unwrap());
+    println!("alloc {:#x?}", alloc_frame());
+    println!("alloc {:#x?}", alloc_frame());
 }
--- os/src/lib.rs
+++ os/src/lib.rs
@@ -11,3 +11,5 @@ mod sbi;
 mod interrupt;
 mod context;
 mod timer;
+mod consts;
+mod memory;
