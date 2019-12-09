1ba5fd7a1d7fc8794583ca1588a262916a20d707
wyfcyx <wyfcyx@gmail.com>
2019-12-09 11:10:05 +0800

    chk-ch2-part4

---
+++ os/src/boot/entry64.asm
@@ -0,0 +1,36 @@
+    .section .text.entry
+    .globl _start
+_start:
+    # 迷之代码
+    lui     t0, %hi(boot_page_table_sv39)
+    li      t1, 0xffffffffc0000000 - 0x80000000
+    sub     t0, t0, t1
+    srli    t0, t0, 12
+    li      t1, 8 << 60
+    or      t0, t0, t1
+    csrw    satp, t0
+    sfence.vma
+    # 迷之代码
+
+    lui sp, %hi(bootstacktop)
+
+    lui t0, %hi(rust_main)
+    addi t0, t0, %lo(rust_main)
+    jr t0
+
+    .section .bss.stack
+    .align 12
+    .global bootstack
+bootstack:
+    .space 4096 * 4
+    .global bootstacktop
+bootstacktop:
+
+    # 迷之代码
+    .section .data
+    .align 12   # page align
+boot_page_table_sv39:
+    # 0xffffffff_c0000000 -> 0x80000000 (1G)
+    .zero 8 * 511
+    .quad (0x80000 << 10) | 0xcf # VRWXAD
+    # 迷之代码
