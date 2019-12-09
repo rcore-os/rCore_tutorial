3b1500685e14d7fe509d8b08e1e5eca4a299b022
wyfcyx <wyfcyx@gmail.com>
2019-12-09 11:21:55 +0800

    chk-ch2-p-part5

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(global_asm)]
+#![feature(asm)]
 
 use core::panic::PanicInfo;
 
@@ -16,8 +17,28 @@ extern "C" fn abort() -> ! {
 
 global_asm!(include_str!("boot/entry64.asm"));
 
+pub fn console_putchar(ch: u8) {
+    let ret: usize;
+    let arg0: usize = ch as usize;
+    let arg1: usize = 0;
+    let arg2: usize = 0;
+    let which: usize = 1;
+    unsafe {
+        asm!("ecall"
+             : "={x10}" (ret)
+             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+             : "memory"
+             : "volatile"
+        );
+    }
+}
+
 #[no_mangle]
-pub extern "C" fn rust_main() -> ! {
+extern "C" fn rust_main() -> ! {
+    // 在屏幕上输出 "OK\n" ，随后进入死循环
+    console_putchar(b'O');
+    console_putchar(b'K');
+    console_putchar(b'\n');
     loop {}
 }
 
