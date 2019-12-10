f408f07523dafd0470ca71826be9d28d82bf2039
wyfcyx <wyfcyx@gmail.com>
2019-12-10 09:09:41 +0800

    chk-ch9-part3

--- os/src/syscall.rs
+++ os/src/syscall.rs
@@ -4,6 +4,7 @@ use crate::process;
 pub const SYS_WRITE: usize = 64;
 pub const SYS_EXIT: usize = 93;
 pub const SYS_READ: usize = 63;
+pub const SYS_EXEC: usize = 221;
 
 pub fn syscall(id: usize, args: [usize; 3], tf: &mut TrapFrame) -> isize {
     match id {
@@ -17,6 +18,9 @@ pub fn syscall(id: usize, args: [usize; 3], tf: &mut TrapFrame) -> isize {
         },
 		SYS_READ => {
             sys_read(args[0], args[1] as *mut u8, args[2])
+        },
+		SYS_EXEC => {
+            sys_exec(args[0] as *const u8)
         },
         _ => {
             panic!("unknown syscall id {}", id);
@@ -33,4 +37,16 @@ fn sys_read(fd: usize, base: *mut u8, len: usize) -> isize {
         *base = crate::fs::stdio::STDIN.pop() as u8;
     }
     return 1;
+}
+
+pub unsafe fn from_cstr(s: *const u8) -> &'static str {
+    use core::{ slice, str };
+    let len = (0usize..).find(|&i| *s.add(i) == 0).unwrap();
+    str::from_utf8(slice::from_raw_parts(s, len)).unwrap()
+}
+
+fn sys_exec(path: *const u8) -> isize {
+    let valid = process::execute(unsafe { from_cstr(path) }, Some(process::current_tid()));
+    if valid { process::yield_now(); }
+    return 0;
 }
