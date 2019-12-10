f83eb30a62fe0a90933853901e38c41b1184b2d2
wyfcyx <wyfcyx@gmail.com>
2019-12-10 09:09:41 +0800

    chk-ch9-part3

---
+++ usr/rust/src/bin/user_shell.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+#[macro_use]
+extern crate user;
+
+const LF: u8 = 0x0au8;
+const CR: u8 = 0x0du8;
+
+use user::io::getc;
+use user::syscall::sys_exec;
+use alloc::string::String;
+
+#[no_mangle]
+pub fn main() {
+   println!("Rust user shell");
+   let mut line: String = String::new();
+   print!(">> ");
+   loop {
+       let c = getc();
+       match c {
+           LF | CR => {
+               println!("");
+               if !line.is_empty() {
+                   println!("searching for program {}", line);
+                   sys_exec(line.as_ptr());
+                   line.clear();
+               }
+               print!(">> ");
+           },
+           _ => {
+ 
+               print!("{}", c as char);
+               line.push(c as char);
+           }
+       }
+   }
+}
