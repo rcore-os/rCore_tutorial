c6519115c68d0c0201af2ebc30fa58af680d691f
PanQL <panqinglin00@163.com>
2020-02-12 20:47:18 +0800

    refactor

--- os/src/memory/page_replace/mod.rs
+++ os/src/memory/page_replace/mod.rs
@@ -1,25 +1,34 @@
-use super::alloc_frame;
-use crate::consts::PHYSICAL_MEMORY_OFFSET;
-use crate::fs::{disk_page_read, disk_page_write};
-use alloc::boxed::Box;
-use alloc::vec::Vec;
-use core::fmt::LowerHex;
-use lazy_static::*;
-use riscv::addr::{Frame, PhysAddr};
-use riscv::paging::{PageTableEntry, PageTableFlags as EF};
-use spin::Mutex;
+use {
+    super::alloc_frame,
+    crate::{
+        consts::{PAGE_SIZE, PHYSICAL_MEMORY_OFFSET},
+        fs::{disk_page_read, disk_page_write},
+    },
+    alloc::boxed::Box,
+    core::fmt::LowerHex,
+    lazy_static::*,
+    riscv::{
+        addr::{Frame, PhysAddr},
+        paging::{PageTableEntry, PageTableFlags as EF},
+    },
+    spin::Mutex,
+};
+
+mod fifo;
+
+pub use fifo::FifoPageReplace;
 
 pub trait PageReplace: Send {
     /// 将可被置换的物理页帧纳入算法
     fn push_frame(&mut self, frame: Frame, pg_entry: usize);
     /// 选择要被置换的物理页帧
-    fn choose_victim(&mut self) -> (Frame, usize);
+    fn choose_victim(&mut self) -> Option<(Frame, usize)>;
     /// 1 复制页帧的内容到磁盘
     /// 2 并记录页帧所在磁盘位置到页表项中
     /// 3 返回可用的物理页帧
     fn swap_out_one(&mut self) -> Frame {
-        let (frame, entry_loc) = self.choose_victim();
-        let swap_page: &mut [u8; (1 << 12)] =
+        let (frame, entry_loc) = self.choose_victim().unwrap();
+        let swap_page: &mut [u8; PAGE_SIZE] =
             unsafe { frame.as_kernel_mut(PHYSICAL_MEMORY_OFFSET) };
         let entry: &mut PageTableEntry = unsafe { &mut *(entry_loc as *mut PageTableEntry) };
         let mut flags = entry.flags().clone();
@@ -34,7 +43,7 @@ pub trait PageReplace: Send {
     fn do_pgfault(&self, entry: &mut PageTableEntry) {
         println!("pgfault addr: {:#x}", entry.addr().as_usize());
         let frame = alloc_frame().unwrap();
-        let new_page: &mut [u8; (1 << 12)] = unsafe { frame.as_kernel_mut(PHYSICAL_MEMORY_OFFSET) };
+        let new_page: &mut [u8; PAGE_SIZE] = unsafe { frame.as_kernel_mut(PHYSICAL_MEMORY_OFFSET) };
         disk_page_read(entry.addr().as_usize(), new_page);
         entry.flags_mut().set(EF::VALID, true);
         let flags = entry.flags();
@@ -44,25 +53,7 @@ pub trait PageReplace: Send {
     fn tick(&self);
 }
 
-pub struct FifoPageReplace {
-    frames: Vec<(Frame, usize)>,
-}
-
-impl PageReplace for FifoPageReplace {
-    fn push_frame(&mut self, frame: Frame, pg_entry: usize) {
-        println!("add frame: {:#x?} pg_entry: {:#x}", frame, pg_entry);
-        self.frames.push((frame, pg_entry));
-    }
-
-    fn choose_victim(&mut self) -> (Frame, usize) {
-        // 选择一个已经分配的物理页帧
-        self.frames.remove(0)
-    }
-
-    fn tick(&self) {}
-}
-
 lazy_static! {
     pub static ref PAGE_REPLACE_HANDLER: Mutex<Box<PageReplace>> =
-        Mutex::new(Box::new(FifoPageReplace { frames: Vec::new() }));
+        Mutex::new(Box::new(FifoPageReplace::default()));
 }
