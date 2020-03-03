390a37f29527d3097531b8659748c3ee7a2cae7e
shinbokuow <wyfcyx@gmail.com>
2020-03-03 22:40:16 +0800

    support backspace

--- usr/rust/src/bin/user_shell.rs
+++ usr/rust/src/bin/user_shell.rs
@@ -8,33 +8,44 @@ extern crate user;
 
 const LF: u8 = 0x0au8;
 const CR: u8 = 0x0du8;
+const DL: u8 = 0x7fu8;
+const BS: u8 = 0x08u8;
 
-use user::io::getc;
-use user::syscall::sys_exec;
 use alloc::string::String;
+use user::io::getc;
+use user::io::putchar;
+use user::syscall::sys_exec;
 
 #[no_mangle]
 pub fn main() {
-   println!("Rust user shell");
-   let mut line: String = String::new();
-   print!(">> ");
-   loop {
-       let c = getc();
-       match c {
-           LF | CR => {
-               println!("");
-               if !line.is_empty() {
-                   println!("searching for program {}", line);
-                   sys_exec(line.as_ptr());
-                   line.clear();
-               }
-               print!(">> ");
-           },
-           _ => {
- 
-               print!("{}", c as char);
-               line.push(c as char);
-           }
-       }
-   }
+    println!("Rust user shell");
+    let mut line: String = String::new();
+    print!(">> ");
+    loop {
+        let c = getc();
+        match c {
+            LF | CR => {
+                println!("");
+                if !line.is_empty() {
+                    println!("searching for program {}", line);
+                    line.push('\0');
+                    sys_exec(line.as_ptr());
+                    line.clear();
+                }
+                print!(">> ");
+            }
+            DL => {
+                if !line.is_empty() {
+                    putchar(BS as char);
+                    putchar(' ');
+                    putchar(BS as char);
+                    line.pop();
+                }
+            }
+            _ => {
+                print!("{}", c as char);
+                line.push(c as char);
+            }
+        }
+    }
 }
