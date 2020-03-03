6b2309db90db111c2223b122822a756794574671
shinbokuow <wyfcyx@gmail.com>
2020-03-03 22:24:02 +0800

    support backspace

--- usr/rust/src/bin/notebook.rs
+++ usr/rust/src/bin/notebook.rs
@@ -5,21 +5,38 @@
 extern crate user;
 
 use user::io::getc;
+use user::io::putchar;
 
 const LF: u8 = 0x0au8;
 const CR: u8 = 0x0du8;
+const BS: u8 = 0x08u8;
+const DL: u8 = 0x7fu8;
 
 #[no_mangle]
 pub fn main() {
     println!("Welcome to notebook!");
+    let mut line_count = 0;
     loop {
         let c = getc();
         match c {
             LF | CR => {
+                line_count = 0;
                 print!("{}", LF as char);
-                print!("{}", CR as char)
+                print!("{}", CR as char);
             }
-            _ => print!("{}", c as char)
+            DL => if line_count > 0 {
+                    putchar(BS as char);
+                    putchar(' ');
+                    putchar(BS as char);
+                    line_count -= 1;
+                }
+            _ => {
+                line_count += 1;
+                print!("{}", c as char);
+                
+            }
+            
         }
+        
     }
 }
